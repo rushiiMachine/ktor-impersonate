dcf8a6c9834ab5cace8d35170fe36a6e7f27b78e
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-09-29 19:53:00 -0700

    feat: rust build setup

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,6 @@
 @file:OptIn(ExperimentalKotlinGradlePluginApi::class)
 
+import com.android.build.gradle.tasks.MergeSourceSetFolders
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
@@ -8,6 +9,7 @@ plugins {
 //	alias(libs.plugins.binaryCompatibilityValidator)
 	alias(libs.plugins.kotlinMultiplatform)
 	alias(libs.plugins.androidLibrary)
+	alias(libs.plugins.rustAGP)
 }
 
 kotlin {
@@ -49,4 +51,32 @@ android {
 		sourceCompatibility = JavaVersion.VERSION_1_8
 		targetCompatibility = JavaVersion.VERSION_1_8
 	}
+
+	ndkVersion = sdkDirectory.resolve("ndk").listFilesOrdered().last().name
+}
+
+cargo {
+	module = "../rust"
+	libname = "ktorimpersonate"
+	targets = listOf("arm", "arm64", "x86", "x86_64")
+	prebuiltToolchains = true
+
+	// This assumes that either `assembleDebug` or `assembleRelease` is used to build
+	gradle.taskGraph.whenReady {
+		if (allTasks.any { it.name.contains("release") })
+			this@cargo.profile = "release"
+	}
+}
+
+afterEvaluate {
+	for (name in arrayOf("mergeDebugJniLibFolders", "mergeReleaseJniLibFolders")) {
+		tasks.getByName(name, MergeSourceSetFolders::class) {
+			dependsOn("cargoBuild")
+			inputs.dir(layout.buildDirectory.asFile.get().resolve("rustJniLibs/android"))
+		}
+	}
+}
+
+tasks.getByName<Delete>("clean") {
+	delete("../rust/target")
 }
